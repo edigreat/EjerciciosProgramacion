e778c297853695298e729596fd72667e08c4eca8
Heriberto G <heriberto.galdamez@gmail.com>
2014-04-28 20:48:09 -0500

    Se agrega primer ejericio de punteros

---
+++ 21Abril2014/01_Punteros.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * Programa para ejemplificar el paso de parametros 
+ * por referencia con punteros en C
+ *
+ * */
+void fnsuma(int *x,int *y);
+
+int main()
+{
+    int x=10,y=20;
+    fnsuma(&x,&y);
+    printf("Resultado = %d \n",x);
+
+    return 0;
+}
+
+/**
+ * Recibe la direccion de las variables
+ */
+
+void fnsuma(int *x,int *y){
+
+    *x = *x + *y;
+
+}
